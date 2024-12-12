51981c5ce6e877ff95aa0037aca6222a2749c6d8
unknown <shourinakhternitul@gmail.com>
2024-12-12 15:35:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// variable declar and value assign 
+// data type: 
+// string data type 
+const userBirthDay = "6-10-2003";
+const userFatherName = "Md Humayun Kabir";
+const userMotherName = "Sahana Kabir";
+const userGender = "Female";
+const userpermanentAddress = "Kali Bari Road";
+const userBloodGroup = "A+";
+const user
+const productName = "computer";
+const userFirstName = "Julykha";
+const userLastName = "Begum";
+const userCountry = "Bangladesh";
+// boolean data type 
+const isAdmin = true;
+// number data type 
+let userClass = "honours";
+let userPrice = 500;
+let userYear = 2024;
+let userPreasentAddress = "kali bari road" ;
+let userHeight = 5.3;
+let userWeight = "50kg";
+let userJob = "company";
+let userNumber = 50;
+let userAge = 22;
+let salary = 12000;
+// underfined data type 
+let firstNumber = 100;
+let secondNumber = 150;
+let city = null;
+console.log(typeof(city));
+// javaScript data type:
+1. primitive data type ---> String, number, boolean, undefined, null, bigInt, symbol
+2. non primitive data type-->arrey, object, function, data, RegExp  
+let friendList = ["Shamim", "Shakil", "Mahim", "Sain", "Sumon", "Sakib"];
+// object declar 
+let userProfile = {
+    id = 202401,
+    name: {
+        firstName : "Julykha",
+        lastName : "Begum",
+    },
+    phoneNumber : "01823320195",
+    address : "Dhaka",
+    education : "Dhaka University",
+}
+console.log(userProfile);
+
---
+++ operators.js
@@ -0,0 +1,11 @@
+// 1. arithmetich operator ---> +, -, *, /, %, ++, --, **
+// 2. assignment operator ---> =,+=, -=, *=, /=, %=, **=
+// 3. comparision operator ---> ==, ===, !=, !==, >, <, <=, >=
+// 4. logical operator ---> &&, ||, !
+// 5. tarnery operator -> ?
+let firstNumber = 3000;
+let secondNumber = 4000;
+let thirdNumber = 5000;
+let totalproductprice = firstNumber + secondNumber + thirdNumber;
+const discountTaka = totalproductprice / 10;
+console.log(discountTaka); 
